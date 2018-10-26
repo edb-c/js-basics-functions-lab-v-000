bd9503a9f98b8b065aa100b516af794935a29d06
Ethel C <cofresi.ethel@gmail.com>
2018-10-26 14:26:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //distanceFromHqInBlocks()
-//returns a distance in blocks ‣
-distanceFromHqInBlocks
-//returns a distance in blocks ‣
+//returns a distance in blocks 
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
+
+//returns a distance in blocks 
 distanceFromHqInBlock
-//calculates distances below 42nd street ‣
+//calculates distances below 42nd street 
 distanceFromHqInBlocks
