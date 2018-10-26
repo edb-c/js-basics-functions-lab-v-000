0de6e373b8161dd085e781187ce8777429aa1ea2
Ethel C <cofresi.ethel@gmail.com>
2018-10-26 14:25:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 //returns a distance in blocks ‣
 distanceFromHqInBlocks
 //returns a distance in blocks ‣
-ReferenceError: distanceFromHqInBlocks is not defined
-    at Context.<anonymous> (test/indexTest.js:10:7)
+distanceFromHqInBlocks
 //calculates distances below 42nd street ‣
 ReferenceError: distanceFromHqInBlocks is not defined
     at Context.<anonymous> (test/indexTest.js:14:7)!
