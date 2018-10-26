9543439ad482503d40fad5873e318803f29be181
Ethel C <cofresi.ethel@gmail.com>
2018-10-26 14:25:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 //returns a distance in blocks ‣
 distanceFromHqInBlocks
 //returns a distance in blocks ‣
-distanceFromHqInBlocks
+distanceFromHqInBlock
 //calculates distances below 42nd street ‣
-ReferenceError: distanceFromHqInBlocks is not defined
-    at Context.<anonymous> (test/indexTest.js:14:7)!
+distanceFromHqInBlocks
